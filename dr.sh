3e6093b23b54aec9a862ae74c2f9d5dac7569a79
lovetwice1012 <59718878+lovetwice1012@users.noreply.github.com>
2021-09-27 15:36:49 +0900

    Create dr.sh

---
+++ dr.sh
@@ -0,0 +1,42 @@
+apt install -y apparmor apport apport-gtk aptdaemon apturl apturl-common command-not-found
+  deja-dup duplicity firefox foomatic-db-compressed-ppds gdm3 gedit
+  gedit-common gnome-control-center gnome-getting-started-docs gnome-menus
+  gnome-online-accounts gnome-shell gnome-shell-extension-appindicator
+  gnome-shell-extension-desktop-icons gnome-shell-extension-ubuntu-dock
+  gnome-terminal gnome-user-docs gvfs-backends hplip hplip-data ibus
+  ibus-table language-selector-common language-selector-gnome libsmbclient
+  lsb-release nautilus-extension-gnome-terminal nautilus-share netplan.io
+  network-manager-gnome networkd-dispatcher openprinting-ppds orca
+  plymouth-theme-spinner plymouth-theme-ubuntu-text printer-driver-foo2zjs
+  printer-driver-foo2zjs-common printer-driver-m2300w
+  printer-driver-postscript-hp printer-driver-ptouch printer-driver-pxljr
+  printer-driver-sag-gdi python-apt-common python3 python3-apport python3-apt
+  python3-aptdaemon python3-aptdaemon.gtk3widgets python3-bcrypt
+  python3-blinker python3-brlapi python3-cairo python3-certifi
+  python3-cffi-backend python3-chardet python3-click python3-colorama
+  python3-commandnotfound python3-cryptography python3-cups
+  python3-cupshelpers python3-dateutil python3-dbus python3-debconf
+  python3-debian python3-defer python3-distro python3-distro-info
+  python3-distupgrade python3-entrypoints python3-fasteners python3-future
+  python3-gdbm python3-gi python3-gi-cairo python3-httplib2 python3-ibus-1.0
+  python3-idna python3-jwt python3-keyring python3-launchpadlib
+  python3-lazr.restfulclient python3-lazr.uri python3-lib2to3 python3-lockfile
+ python3-louis python3-macaroonbakery python3-mako python3-markupsafe
+  python3-minimal python3-monotonic python3-nacl python3-netifaces
+  python3-oauthlib python3-olefile python3-paramiko python3-pexpect
+  python3-pil python3-pkg-resources python3-problem-report python3-protobuf
+  python3-ptyprocess python3-pyatspi python3-pymacaroons python3-renderpm
+  python3-reportlab python3-reportlab-accel python3-requests
+  python3-requests-unixsocket python3-rfc3339 python3-secretstorage
+  python3-simplejson python3-six python3-software-properties python3-speechd
+  python3-systemd python3-talloc python3-tz python3-uno python3-update-manager
+  python3-urllib3 python3-wadllib python3-xdg python3-xkit python3-yaml
+  python3.8 python3.8-minimal rhythmbox-plugin-alternative-toolbar
+  rhythmbox-plugins samba-libs snapd software-properties-common
+  software-properties-gtk system-config-printer system-config-printer-common
+  system-config-printer-udev totem-plugins ubuntu-advantage-tools
+  ubuntu-desktop ubuntu-desktop-minimal ubuntu-docs ubuntu-drivers-common
+  ubuntu-minimal ubuntu-release-upgrader-core ubuntu-release-upgrader-gtk
+  ubuntu-session ubuntu-standard ufw unattended-upgrades update-manager
+  update-manager-core update-notifier update-notifier-common
+  usb-creator-common usb-creator-gtk xorg xserver-xorg yelp
